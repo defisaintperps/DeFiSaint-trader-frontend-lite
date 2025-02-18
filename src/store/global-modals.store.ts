262e866ea8a4c7c0906b1904b3c72ba4630884ba
m66260 <m66260@protonmail.com>
2025-02-18 11:26:37 -0500

    open different modals conditionally

--- src/store/global-modals.store.ts
+++ src/store/global-modals.store.ts
@@ -1,10 +1,11 @@
 import { atom } from 'jotai';
 
 import { CurrencyItemI } from '../components/currency-selector/types';
+import { flatTokenAtom, selectedPoolAtom } from './pools.store';
 
 export const accountModalOpenAtom = atom(false);
 export const modalSelectedCurrencyAtom = atom<CurrencyItemI | null>(null);
-export const depositModalOpenAtom = atom(false);
+
 export const oneClickModalOpenAtom = atom(false);
 export const connectModalOpenAtom = atom(false);
 export const cedeModalOpenAtom = atom(false);
@@ -16,3 +17,20 @@ export const takeProfitModalOpenAtom = atom(false);
 export const marketSelectModalOpenAtom = atom(false);
 export const tradeHistoryModalOpenAtom = atom(false);
 export const flatTokentModalOpenAtom = atom(false);
+
+const depositModalOpenPrimitiveAtom = atom(false);
+export const depositModalOpenAtom = atom(
+  (get) => get(depositModalOpenPrimitiveAtom),
+  (get, set, value: boolean) => {
+    const flatToken = get(flatTokenAtom);
+    const selectedPool = get(selectedPoolAtom);
+    if (value && flatToken?.isFlatToken && !flatToken?.registeredToken && selectedPool?.poolId === flatToken.poolId) {
+      console.log({ value, flatToken, selectedPoolId: selectedPool?.poolId });
+      set(flatTokentModalOpenAtom, true);
+      set(depositModalOpenPrimitiveAtom, false);
+    } else {
+      console.log({ value, flatToken, selectedPoolId: selectedPool?.poolId });
+      set(depositModalOpenPrimitiveAtom, value);
+    }
+  }
+);
